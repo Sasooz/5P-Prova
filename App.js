6c752c7b3a23a8dcc5ca551854da0c69188330c2
Sasooz <savioalessandro13042005@gmail.com>
2025-04-30 20:27:41 -0300

    Prova Completa

--- App.js
+++ App.js
@@ -1,11 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Keyboard,
+} from "react-native";
 
 export default function App() {
+
+  const [T1, setT1] = useState("");
+  const [T2, setT2] = useState("");
+  const [P1, setP1] = useState("");
+  const [P2, setP2] = useState("");
+  const [notaFinal, setNotaFinal] = useState(null);
+  const [Resultado, setResultado] = useState("");
+
+  const CalcularNotaFinal = () => {
+    try {
+    
+      const NotaTrabalho =  parseFloat(T1) + parseFloat(T2);
+      const NotaProva = parseFloat(P1) + parseFloat(P2);
+      const NotaMedia = (NotaTrabalho + NotaProva) / 2
+      console.log("NotaMedia", NotaMedia)
+
+      if (NotaMedia < 7) {
+        setResultado("Aluno está de P3 e precisa tirar " + (12 - NotaMedia) + " pontos para ser aprovado.")
+      } else {
+        setResultado("Aluno foi aprovado com " + NotaMedia + " pontos.")
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Nota do Trabalho 1:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={T1}
+        onChangeText={setT1}
+        placeholder="Digite sua Nota de Trabalho"
+      />
+
+<Text style={styles.label}>Nota do Trabalho 2:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={T2}
+        onChangeText={setT2}
+        placeholder="Digite sua Nota de Trabalho"
+      />
+
+<Text style={styles.label}>Nota da Prova 1:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={P1}
+        onChangeText={setP1}
+        placeholder="Digite sua Nota da Prova"
+      />
+
+<Text style={styles.label}>Nota da Prova 2:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={P2}
+        onChangeText={setP2}
+        placeholder="Digite sua Nota da Prova"
+      />
+
+      <TouchableOpacity style={styles.botao} onPress={CalcularNotaFinal}>
+        <Text style={styles.botaoTexto}>Calcular Nota</Text>
+      </TouchableOpacity>
+      <Text>
+        {Resultado}
+      </Text>
     </View>
   );
 }
@@ -13,8 +86,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    padding: 40,
+    backgroundColor: "#FFF",
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 20,
+    fontSize: 16,
+  },
+  botao: {
+    backgroundColor: "#FFD700",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 40,
+  },
+  botaoTexto: {
+    fontSize: 18,
+    color: "#000",
+    fontWeight: "bold",
+  },
+  notaFinal: {
+    fontSize: 18,
+    color: "#000",
+    fontWeight: "bold"
   },
 });
---
+++ src/components/CalculaNota.js
@@ -0,0 +1,17 @@
+// export default function CalculaNota(P1,P2,T1,T2) {
+
+//   const P1 = parseFloat(P1);
+//   const P2 = parseFloat(P2);
+//   const T1 = parseFloat(T1);
+//   const T2 = parseFloat(T2);
+
+//   if (isNaN() || T1 && T2 && P1 && P2 <= 0) {
+//     throw new Error("Por favor, insira uma nota válida");
+//   }
+
+//   const NotaTrabalho =  T1 + T2;
+//   const NotaProva = P1 + P2;
+//   const NotaMedia = (NotaTrabalho + NotaProva) / 2
+
+//   return NotaMedia
+// }
